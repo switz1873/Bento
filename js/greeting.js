a224f995c9e7e605b9a5151b44367718e4dd6818
switz1873 <34579403+switz1873@users.noreply.github.com>
2020-08-15 18:04:52 -0400

    Update greeting.js
    Removed name from greetings

--- js/greeting.js
+++ js/greeting.js
@@ -2,18 +2,18 @@ var today = new Date();
 var Hr = today.getHours();
 
 // Here you can change the greetings and your name
-var name = 'John Doe';
+//var name = 'John Doe';
 var lateTxt = 'Go to Sleep! ';
 var morningTxt = 'Good morning! ';
 var afterTxt = 'Good afternoon ';
 var evenTxt = 'Good evening ';
 
 if (Hr >= 0 && Hr < 6) {
-    document.getElementById('greetings').innerText = lateTxt + name;
+    document.getElementById('greetings').innerText = lateTxt;
 } else if (Hr >= 6 && Hr < 12) {
-    document.getElementById('greetings').innerText = morningTxt + name;
+    document.getElementById('greetings').innerText = morningTxt;
 } else if (Hr >= 12 && Hr < 17) {
-    document.getElementById('greetings').innerText = afterTxt + name;
+    document.getElementById('greetings').innerText = afterTxt;
 } else {
-    document.getElementById('greetings').innerText = evenTxt + name;
+    document.getElementById('greetings').innerText = evenTxt;
 }
